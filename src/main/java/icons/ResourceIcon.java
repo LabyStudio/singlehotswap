966ddc3c286f790a532c9a276b450f8d29bcc003
LabyStudio <labystudio@gmail.com>
2024-08-29 15:58:03 +0200

    support versions below 2023.2.7, v2.9

--- src/main/java/icons/ResourceIcon.java
+++ src/main/java/icons/ResourceIcon.java
@@ -5,13 +5,27 @@ import com.intellij.ui.IconManager;
 import javax.swing.Icon;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.lang.reflect.Method;
 
 public class ResourceIcon implements Icon {
 
     private final Icon baseIcon;
 
     public ResourceIcon(String path) {
-        this.baseIcon = IconManager.getInstance().getIcon(path, ResourceIcon.class.getClassLoader());
+        IconManager manager = IconManager.getInstance();
+        Icon baseIcon;
+        try {
+            baseIcon = manager.getIcon(path, ResourceIcon.class.getClassLoader());
+        } catch (Throwable e) {
+            // Support for older versions of IntelliJ IDEA (before 2023.2.7)
+            try {
+                Method getIcon = manager.getClass().getDeclaredMethod("getIcon", String.class, Class.class);
+                baseIcon = (Icon) getIcon.invoke(manager, path, ResourceIcon.class);
+            } catch (Throwable ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        this.baseIcon = baseIcon;
     }
 
     @Override
